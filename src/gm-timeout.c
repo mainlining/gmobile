885f24837c08eb4ebdf2156e01bf6786a2e05e1f
Guido Günther <agx@sigxcpu.org>
2022-10-01 18:52:58 +0200

    timeout: Reformat according to coding style

--- src/gm-timeout.c
+++ src/gm-timeout.c
@@ -14,11 +14,11 @@
 
 
 typedef struct _GmTimeoutOnce {
-  GSource   source;
-  int       fd;
-  gpointer  tag;
-  gulong    timeout_ms;
-  gboolean  armed;
+  GSource  source;
+  int      fd;
+  gpointer tag;
+  gulong   timeout_ms;
+  gboolean armed;
 } GmTimeoutOnce;
 
 
@@ -41,28 +41,29 @@ gm_timeout_once_prepare (GSource *source, gint *timeout)
 
   ret = timerfd_settime (timer->fd, 0 /* flags */, &time_spec, NULL);
   if (ret)
-    g_warning ("Failed to set up timer: %s", strerror(ret));
+    g_warning ("Failed to set up timer: %s", strerror (ret));
 
   g_debug ("Prepared %p", source);
   timer->armed = TRUE;
+  /* Never wake up the source due to a timeout */
   *timeout = -1;
   return FALSE;
 }
 
 
 static gboolean
-gm_timeout_once_dispatch (GSource     *source,
-			     GSourceFunc  callback,
-			     void        *data)
+gm_timeout_once_dispatch (GSource    *source,
+                          GSourceFunc callback,
+                          void       *data)
 {
   if (!callback) {
     g_warning ("Timeout source dispatched without callback. "
-	       "You must call g_source_set_callback().");
+               "You must call g_source_set_callback().");
     return G_SOURCE_REMOVE;
   }
 
   g_debug ("dispatch %p", source);
-  callback(data);
+  callback (data);
 
   return G_SOURCE_REMOVE;
 }
@@ -73,7 +74,7 @@ gm_timeout_once_finalize (GSource *source)
 {
   GmTimeoutOnce *timer = (GmTimeoutOnce *) source;
 
-  close(timer->fd);
+  close (timer->fd);
   timer->fd = -1;
   timer->armed = FALSE;
 
@@ -97,7 +98,7 @@ gm_timeout_source_once_new (gulong timeout_ms)
 {
   int fdf, fsf;
   GmTimeoutOnce *timer = (GmTimeoutOnce *) g_source_new (&gm_timeout_once_source_funcs,
-						   sizeof (GmTimeoutOnce));
+                                                   sizeof (GmTimeoutOnce));
 
   timer->timeout_ms = timeout_ms;
 #if GLIB_CHECK_VERSION(2, 70, 0)
@@ -140,10 +141,10 @@ gm_timeout_source_once_new (gulong timeout_ms)
  **/
 guint
 gm_timeout_add_seconds_once_full (gint           priority,
-				  gulong         seconds,
-				  GSourceFunc    function,
-				  gpointer       data,
-				  GDestroyNotify notify)
+                                  gulong         seconds,
+                                  GSourceFunc    function,
+                                  gpointer       data,
+                                  GDestroyNotify notify)
 {
   g_autoptr (GSource) source = NULL;
   guint id;
@@ -182,9 +183,9 @@ gm_timeout_add_seconds_once_full (gint           priority,
  * Returns: the ID (greater than 0) of the event source.
  **/
 guint
-gm_timeout_add_seconds_once (int          seconds,
-			     GSourceFunc  function,
-			     gpointer     data)
+gm_timeout_add_seconds_once (int         seconds,
+                             GSourceFunc function,
+                             gpointer    data)
 {
   g_return_val_if_fail (function != NULL, 0);
 
